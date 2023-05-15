29dc426c1c08add66eb5bfa47a856a0effac2b15
ngcheukchi37 <ngcheukchi37@gmail.com>
2023-05-16 00:39:40 +0800

    Git init

---
+++ client/src/pages/MetaversePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const MetaversePage = () => {
+  return (
+  <div>
+  <iframe src="http://127.0.0.1:8887/" width="100%" height="700px"></iframe>
+  </div>  
+  )
+}
+export default MetaversePage;
